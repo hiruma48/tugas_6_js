293df22e8cd5f29244f3fdad688a52dd6d39ad1d
tri ariyanto <triyanto129@gmail.com>
2023-05-06 14:20:50 +0700

    tugas 6 js
    mengumpulkan tugas

---
+++ tugas6.js/tugas6.js
@@ -0,0 +1,12 @@
+function perulangan(){
+    for (var i = 0;i <=20; i++){
+        if(i%2===0){
+            console.log("genap")
+        }
+        else{
+            console.log(i)
+        }
+    }
+}
+
+perulangan()
